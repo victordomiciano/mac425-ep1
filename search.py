2598698e9588532797b8fe47f8a2e8bbcf83c41b
Victor Domiciano <victor.dm3@gmail.com>
2018-04-08 22:19:12 -0300

    Add depth first search

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,6 +18,12 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+
+from game import Directions
+n = Directions.NORTH
+s = Directions.SOUTH
+e = Directions.EAST
+w = Directions.WEST
 
 class SearchProblem:
     """
@@ -67,10 +73,7 @@ def tinyMazeSearch(problem):
     Returns a sequence of moves that solves tinyMaze.  For any other maze, the
     sequence of moves will be incorrect, so only use this for tinyMaze.
     """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
     """
@@ -86,8 +89,53 @@ def depthFirstSearch(problem):
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    def getPredecessor(actualState):
+        if actualState[1] == 'North':
+            return (actualState[0][0], actualState[0][1] - 1)
+        if actualState[1] == 'South':
+            return (actualState[0][0], actualState[0][1] + 1)
+        if actualState[1] == 'East':
+            return (actualState[0][0] - 1, actualState[0][1])
+        if actualState[1] == 'West':
+            return (actualState[0][0] + 1, actualState[0][1])
+
+    def pushStack(state):
+        util.Stack.push(stack, state[0])
+        dirlist = list(coord.get(getPredecessor(state)))
+        dirlist.append(state[1])
+        coord[state[0]] = dirlist
+
+    visited = [problem.getStartState()]
+    if problem.isGoalState(visited[0]):
+        return []
+    stack = util.Stack()
+    coord = {}
+    goal = (-1, -1)
+
+    successors = problem.getSuccessors(problem.getStartState())
+    pos = problem.getStartState()
+    coord[pos] = []
+
+    for i in range(len(successors)):
+        pushStack(successors[i])
+    while (not(util.Stack.isEmpty(stack)) and goal == (-1, -1)):
+        successors = problem.getSuccessors(util.Stack.pop(stack))
+        for i in range(len(successors)):
+            for j in range(len(visited)):
+                if visited[j] == successors[i][0]:
+                    valid = 0
+                    break
+            if valid == 0:
+                valid = 1
+                continue
+            visited.append(successors[i][0])
+            pushStack(successors[i])
+            if problem.isGoalState(successors[i][0]):
+                goal = successors[i][0]
+                break
+
+    return coord.get(goal)
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
